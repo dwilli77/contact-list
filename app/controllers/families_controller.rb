b5a4db1301c2ab09156b3fb1a3c1d94a06206577
Derek Williams <dwilli77@gmail.com>
2019-08-21 10:41:45 -0400

    added styling

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -28,8 +28,8 @@ class FamiliesController < ApplicationController
 
     respond_to do |format|
       if @family.save
-        format.html { redirect_to @family, notice: 'Family was successfully created.' }
-        format.json { render :show, status: :created, location: @family }
+        format.html { redirect_to families_url, notice: 'Family was successfully created.' }
+        format.json { render :index, status: :created, location: @family }
       else
         format.html { render :new }
         format.json { render json: @family.errors, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class FamiliesController < ApplicationController
   def update
     respond_to do |format|
       if @family.update(family_params)
-        format.html { redirect_to @family, notice: 'Family was successfully updated.' }
+        format.html { redirect_to families_url, notice: 'Family was successfully updated.' }
         format.json { render :show, status: :ok, location: @family }
       else
         format.html { render :edit }
